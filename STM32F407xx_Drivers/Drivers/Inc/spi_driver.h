154ba3c1a6d45930b18e04c8b5ca2dcacf06b77d
Van Tung Dinh <122477565+dinh-van-tung@users.noreply.github.com>
2025-06-19 23:23:43 +0700

    Update spi_driver.h

--- STM32F407xx_Drivers/Drivers/Inc/spi_driver.h
+++ STM32F407xx_Drivers/Drivers/Inc/spi_driver.h
@@ -12,18 +12,18 @@
 #include "stm32f407xx.h"
 
 typedef struct {
-	volatile uint8_t SPI_DeviceMode;
-	volatile uint8_t SPI_BusConfig;
-	volatile uint8_t SPI_DFF;
-	volatile uint8_t SPI_CPHA;
-	volatile uint8_t SPI_CPOL;
-	volatile uint8_t SPI_SSM;
-	volatile uint8_t SPI_SCLKSpeed;
+    volatile uint8_t SPI_DeviceMode;
+    volatile uint8_t SPI_BusConfig;
+    volatile uint8_t SPI_DFF;
+    volatile uint8_t SPI_CPHA;
+    volatile uint8_t SPI_CPOL;
+    volatile uint8_t SPI_SSM;
+    volatile uint8_t SPI_SCLKSpeed;
 } SPI_Config_t;
 
 typedef struct {
-	volatile SPI_RegDef_t *pSPIx;
-	volatile SPI_Config_t SPI_Config;
+    volatile SPI_RegDef_t *pSPIx;
+    volatile SPI_Config_t SPI_Config;
 } SPI_Handle_t;
 
 #define SPI_DEVICE_MODE_SLAVE 0
@@ -72,9 +72,11 @@ void SPI_Init(volatile SPI_Handle_t *pSPI_Handle);
 
 void SPI_DeInit(volatile SPI_RegDef_t *pSPIx);
 
-void SPI_SendData(volatile SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t LengthData);
+void SPI_SendData(volatile SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer,
+        uint32_t LengthData);
 
-void SPI_ReceiveData(volatile SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t LengthData);
+void SPI_ReceiveData(volatile SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer,
+        uint32_t LengthData);
 
 void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t ENorDI);
 
