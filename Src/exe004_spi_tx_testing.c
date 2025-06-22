aec9a406042a7c824da11ac3b34517533bdc6881
Van Tung Dinh <122477565+dinh-van-tung@users.noreply.github.com>
2025-06-22 16:46:58 +0700

    Update exe004_spi_tx_testing.c

--- Src/exe004_spi_tx_testing.c
+++ Src/exe004_spi_tx_testing.c
@@ -11,6 +11,11 @@
 #include "stm32f407xx.h"
 
 
+void TimeDelay(uint8_t Div_x) {
+	for (uint32_t i = 0; i <= 500000 / Div_x; i += 1) {}
+}
+
+
 /**
  * PB12 -> SPI2_NSS
  * PB13	-> SPI2_SCK
@@ -18,7 +23,14 @@
  * PB15 -> SPI2_MOSI
  */
 void SPI2_GPIO_Inits(void) {
-	GPIO_Handle_t SPI2_GPIO_Handle;
+	GPIO_Handle_t SPI2_GPIO_Handle, Button;
+
+	Button.pGPIOx = GPIOA;
+	Button.GPIO_Config.GPIO_PinMode = GPIO_MODE_INPUT;
+	Button.GPIO_Config.GPIO_PinPuPdControl = GPIO_NOPULL;
+	Button.GPIO_Config.GPIO_PinNumber = GPIO_PIN_0;
+
+	GPIO_Init(&Button);
 
 	SPI2_GPIO_Handle.pGPIOx = GPIOB;
 	SPI2_GPIO_Handle.GPIO_Config.GPIO_PinMode = GPIO_MODE_ALTFUNC;
@@ -50,11 +62,11 @@ void SPI2_Inits(void) {
 	SPI2_Handle.pSPIx = SPI2;
 	SPI2_Handle.SPI_Config.SPI_BusConfig = SPI_BUS_CONFIG_FULL_DUPLEX;
 	SPI2_Handle.SPI_Config.SPI_DeviceMode = SPI_DEVICE_MODE_MASTER;
-	SPI2_Handle.SPI_Config.SPI_SCLKSpeed = SPI_SCLK_SPEED_DIV_2;
+	SPI2_Handle.SPI_Config.SPI_SCLKSpeed = SPI_SCLK_SPEED_DIV_32;
 	SPI2_Handle.SPI_Config.SPI_DFF = SPI_DFF_8_BITS;
 	SPI2_Handle.SPI_Config.SPI_SSM = SPI_SSM_SOFTWARE_EN;
-	SPI2_Handle.SPI_Config.SPI_CPOL = SPI_CPOL_LOW;					/* App Logic 2: CPOL = 0 ??? */
-	SPI2_Handle.SPI_Config.SPI_CPHA = SPI_CPHA_FIRST_CLK;			/* App Logic 2: CPHA = 1 ??? */
+	SPI2_Handle.SPI_Config.SPI_CPOL = SPI_CPOL_LOW;
+	SPI2_Handle.SPI_Config.SPI_CPHA = SPI_CPHA_FIRST_CLK;
 	SPI_Init(&SPI2_Handle);
 }
 
@@ -62,20 +74,23 @@ void SPI2_Inits(void) {
 int main(void) {
 	SPI2_GPIO_Inits();
 	SPI2_Inits();
-
-	GPIO_WriteOutputPin(GPIOB, GPIO_PIN_13, LOW);
-	GPIO_WriteOutputPin(GPIOB, GPIO_PIN_15, LOW);
-
-	char user_data[] = "Hello World ?";
-
 	SPI_SSIConfig(SPI2, ENABLE);
 	SPI_SPEConfig(SPI2, ENABLE);
 
-	SPI_SendData(SPI2, (uint8_t*)user_data, strlen(user_data));
+	char user_data[] = "Master -> Slave: Hello SPI World <3";
 
-	SPI_SPEConfig(SPI2, DISABLE);
+//	TimeDelay(2);
+//	SPI_SendData(SPI2, (uint8_t*)user_data, strlen(user_data));
+//
+//	SPI_SPEConfig(SPI2, DISABLE);
+//
+//	while (1) {}
 
-	while(1) {}
+	while (1) {
+		while (GPIO_ReadInputPin(GPIOA, GPIO_PIN_0) == LOW) {}
+		TimeDelay(2);
+		SPI_SendData(SPI2, (uint8_t*)user_data, strlen(user_data));
+	}
 
 	return 0;
 }
