263589709e646be4b307c966be2bfc9d25345923
Van Tung Dinh <122477565+dinh-van-tung@users.noreply.github.com>
2025-06-25 21:06:35 +0700

    Update stm32f407xx.h

--- Drivers/Inc/stm32f407xx.h
+++ Drivers/Inc/stm32f407xx.h
@@ -1,7 +1,7 @@
 /**
  * stm32f407xx.h
  *
- * Created on: Jun 20, 2025
+ * Last reviewed and updated: 2025/06/25
  * Author: VanTungDinh
  */
 
@@ -20,13 +20,16 @@
  * Brief description:               None
  */
 #define NVIC_BASE           0xE000E100UL
+
 #define FLASH_BASE          0x08000000UL
+#define SRAM1_SIZE          (112 * 1024)                    /* 112 KB * 1024 = 114688 B */
 #define SRAM1_BASE          0x20000000UL
-#define SRAM1_SIZE          (112 * 1024)                    /* 112 KB * 1024 = 114688 Byte */
-#define SRAM2_BASE          (SRAM1_BASE + SRAM1_SIZE)
+#define SRAM2_SIZE			(16 * 1024)						/* 16 KB * 1024 = 16384 B */
+#define SRAM2_BASE          0x2001C000UL
 #define SRAM_BASE           SRAM1_BASE
 #define ROM_BASE            0x1FFF0000UL                    /* System memory */
-#define PERIPHERAL_BASE     0x40000000UL
+
+#define PERIPHERAL_BASE		0x40000000UL
 #define APB1_BASE           0x40000000UL
 #define APB2_BASE           0x40010000UL
 #define AHB1_BASE           0x40020000UL
@@ -41,7 +44,7 @@
  * Return type:                     None
  * Brief description:               None
  */
-#define GPIOA_BASE      (AHB1_BASE + 0x0000)
+#define GPIOA_BASE		(AHB1_BASE + 0x0000)
 #define GPIOB_BASE      (AHB1_BASE + 0x0400)
 #define GPIOC_BASE      (AHB1_BASE + 0x0800)
 #define GPIOD_BASE      (AHB1_BASE + 0x0C00)
@@ -60,15 +63,15 @@
  * Return type:                     None
  * Brief description:               None
  */
-#define I2C1_BASE        (APB1_BASE + 0x5400)
-#define I2C2_BASE        (APB1_BASE + 0x5800)
-#define I2C3_BASE        (APB1_BASE + 0x5C00)
-#define SPI2_BASE        (APB1_BASE + 0x3800)
-#define SPI3_BASE        (APB1_BASE + 0x3C00)
-#define USART2_BASE      (APB1_BASE + 0x4400)
-#define USART3_BASE      (APB1_BASE + 0x4800)
-#define UART4_BASE       (APB1_BASE + 0x4C00)
-#define UART5_BASE       (APB1_BASE + 0x5000)
+#define I2C1_BASE        	(APB1_BASE + 0x5400)
+#define I2C2_BASE        	(APB1_BASE + 0x5800)
+#define I2C3_BASE        	(APB1_BASE + 0x5C00)
+#define SPI2_BASE        	(APB1_BASE + 0x3800)
+#define SPI3_BASE        	(APB1_BASE + 0x3C00)
+#define USART2_BASE			(APB1_BASE + 0x4400)
+#define USART3_BASE			(APB1_BASE + 0x4800)
+#define UART4_BASE       	(APB1_BASE + 0x4C00)
+#define UART5_BASE       	(APB1_BASE + 0x5000)
 
 
 /**
@@ -78,11 +81,11 @@
  * Return type:                     None
  * Brief description:               None
  */
-#define SPI1_BASE        (APB2_BASE + 0x3000)
-#define USART1_BASE      (APB2_BASE + 0x1000)
-#define USART6_BASE      (APB2_BASE + 0x1400)
-#define EXTI_BASE        (APB2_BASE + 0x3C00)
-#define SYSCFG_BASE      (APB2_BASE + 0x3800)
+#define SPI1_BASE        	(APB2_BASE + 0x3000)
+#define USART1_BASE			(APB2_BASE + 0x1000)
+#define USART6_BASE      	(APB2_BASE + 0x1400)
+#define EXTI_BASE        	(APB2_BASE + 0x3C00)
+#define SYSCFG_BASE			(APB2_BASE + 0x3800)
 
 
 /**
@@ -230,7 +233,7 @@ typedef struct {
     volatile uint32_t SR; 				/* SPI status register                      - Address offset: 0x08 */
     volatile uint32_t DR; 				/* SPI data register                        - Address offset: 0x0C */
     volatile uint32_t CRCPR;			/* SPI CRC polynomial register              - Address offset: 0x10 */
-    volatile uint32_t RXCRCR; 			/* SPI RX CRC register                      - Address offset: 0x14 */
+    volatile uint32_t RXCRCR;			/* SPI RX CRC register                      - Address offset: 0x14 */
     volatile uint32_t TXCRCR; 			/* SPI TX CRC register                      - Address offset: 0x18 */
     volatile uint32_t I2SCFGR;			/* SPI_I2S configuration register           - Address offset: 0x1C */
     volatile uint32_t I2SPR; 			/* SPI_I2S prescaler register               - Address offset: 0x20 */
